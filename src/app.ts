43db7bdf800681bcda4762b566e6ef1ede73a6e2
Raihan Pratama Putra <98379156+Xizuu@users.noreply.github.com>
2025-01-22 13:22:30 +0800

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET','POST','PUT','DELETE'],
+    allowedHeaders: ['Content-Type','Authorization']
+}));
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
